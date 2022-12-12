4c05dd40f5f6454962f5e1fca09f903cb08f1e02
Arild-BM <112947008+Arild-BM@users.noreply.github.com>
2022-12-12 14:18:27 +0100

    Add files via upload

--- components/Meme.js
+++ components/Meme.js
@@ -18,7 +18,7 @@ export default function Meme() {
         const randomNumber = Math.floor(Math.random() * allMemes.length)
         const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
-            // ...prevMeme,
+            ...prevMeme,
             randomImage: url
         }))
         
@@ -27,7 +27,7 @@ export default function Meme() {
     function handleChange(event) {
         const {name, value} = event.target
         setMeme(prevMeme => ({
-            // ...prevMeme,
+            ...prevMeme,
             [name]: value
         }))
     }
